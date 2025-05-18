00b99223e140c97b9c1c628245ca0e5dacdd8976
Dawid Zubrowski <dawid.zubrowski@vattenfall.com>
2025-05-18 23:29:56 +0200

    fixed problem with big spacing between title and chips

--- lib/ui/core/ui/collections/chips_screen_view.dart
+++ lib/ui/core/ui/collections/chips_screen_view.dart
@@ -9,27 +9,25 @@ class ChipsCollectionView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding:
-              AppTheme.horizontalPadding(),
-          child: Wrap(
-            spacing: AppTheme.columnSpacingSmall,
-            runSpacing: AppTheme.rowSpacingSmall,
-            alignment:
-                WrapAlignment.center,
-            children: items.map((item) {
-              return Chip(
-                label: Text(
-                  item,
-                  style: const TextStyle(color: Colors.black),
-                ),
-                backgroundColor: Colors.white,
-                shape: const StadiumBorder(),
-                side: const BorderSide(color: Colors.black, width: 1),
-              );
-            }).toList(),
-          ),
+      body: Padding(
+        padding:
+            AppTheme.horizontalPadding(),
+        child: Wrap(
+          spacing: AppTheme.columnSpacingSmall,
+          runSpacing: AppTheme.rowSpacingSmall,
+          alignment:
+              WrapAlignment.center,
+          children: items.map((item) {
+            return Chip(
+              label: Text(
+                item,
+                style: const TextStyle(color: Colors.black),
+              ),
+              backgroundColor: Colors.white,
+              shape: const StadiumBorder(),
+              side: const BorderSide(color: Colors.black, width: 1),
+            );
+          }).toList(),
         ),
       ),
     );
