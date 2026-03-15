38042f48b6a27b5e6e6c1444e5599530737012de
Dawid Zubrowski <dawid.zubrowski.i@gmail.com>
2026-03-15 13:20:58 +0100

    fixed tests

--- test/data/profile/repository/profiles_repository_test.dart
+++ test/data/profile/repository/profiles_repository_test.dart
@@ -2,12 +2,12 @@ import 'package:drift/drift.dart' hide isNull, isNotNull;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:sport_matcher/data/profile/domain/profile_domain.dart';
+import 'package:sport_matcher/data/profile/config/activities_config.dart';
 import 'package:sport_matcher/data/profile/persistence/database/abstract_profile_database.dart';
 import 'package:sport_matcher/data/profile/persistence/database/profile_database.dart';
 import 'package:sport_matcher/data/profile/repository/profiles_repository.dart';
-import 'package:uuid/uuid.dart';
 
+import '../../../random/profile_domain_random.dart';
 import '../../../random/profile_entity_data_random.dart';
 import 'profiles_repository_test.mocks.dart';
 
@@ -26,19 +26,30 @@ void main() {
 
     test('addProfile inserts mapped companion', () async {
       // given
-      final profile = ProfileDomain(Uuid().v4().toString());
       when(profileDatabase.insertProfile(any)).thenAnswer((_) async => 1);
+      final profile = ProfileDomainRandom.random();
 
       // when
       await sut.addProfile(profile);
 
       // then
-      final captured = verify(profileDatabase.insertProfile(captureAny))
-          .captured
-          .single as ProfileEntityCompanion;
+      final captured =
+          verify(profileDatabase.insertProfile(captureAny)).captured.single
+              as ProfileEntityCompanion;
 
       final expectedProfile = ProfileEntityCompanion(
         name: Value(profile.name),
+        profileImagePath: Value(profile.profileImagePath),
+        bike: Value(profile.activities[ActivitiesConfig.bike] ?? false),
+        climbing: Value(profile.activities[ActivitiesConfig.climbing] ?? false),
+        football: Value(profile.activities[ActivitiesConfig.football] ?? false),
+        hockey: Value(profile.activities[ActivitiesConfig.hockey] ?? false),
+        pingPong: Value(profile.activities[ActivitiesConfig.pingPong] ?? false),
+        running: Value(profile.activities[ActivitiesConfig.running] ?? false),
+        tennis: Value(profile.activities[ActivitiesConfig.tennis] ?? false),
+        voleyball: Value(
+          profile.activities[ActivitiesConfig.voleyball] ?? false,
+        ),
       );
       expect(expectedProfile, captured);
     });
@@ -57,19 +68,21 @@ void main() {
       verify(profileDatabase.loadProfile()).called(1);
     });
 
-    test('loadProfile maps entity to domain when database returns entity',
-        () async {
-      // given
-      final entity = ProfileEntityDataRandom.random();
-      when(profileDatabase.loadProfile()).thenAnswer((_) async => entity);
+    test(
+      'loadProfile maps entity to domain when database returns entity',
+      () async {
+        // given
+        final entity = ProfileEntityDataRandom.random();
+        when(profileDatabase.loadProfile()).thenAnswer((_) async => entity);
 
-      // when
-      final result = await sut.loadProfile();
+        // when
+        final result = await sut.loadProfile();
 
-      // then
-      expect(result, isNotNull);
-      expect(result!.name, entity.name);
-      verify(profileDatabase.loadProfile()).called(1);
-    });
+        // then
+        expect(result, isNotNull);
+        expect(result!.name, entity.name);
+        verify(profileDatabase.loadProfile()).called(1);
+      },
+    );
   });
 }
