7328f33ce1f0c49645ed5a1becae54a768eacc17
Dawid Zubrowski <aurochs108@gmail.com>
2026-03-23 23:20:49 +0100

    test: rewrite CreatedProfileScreenModel tests for current API

--- test/ui/profile/created_profile/widgets/created_profile_screen_model_test.dart
+++ test/ui/profile/created_profile/widgets/created_profile_screen_model_test.dart
@@ -1,10 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:sport_matcher/data/profile/repository/abstract_profiles_repository.dart';
 import 'package:sport_matcher/ui/profile/created_profile/widgets/created_profile_screen_model.dart';
+import 'package:sport_matcher/ui/profile/edit_profile/widgets/edit_profile_screen.dart';
 
+import '../../../../mocks/mock_navigator_observer.dart';
 import '../../../../random/profile_domain_random.dart';
+import '../../../../utilities/build_context_provider.dart';
 import 'created_profile_screen_model_test.mocks.dart';
 
 @GenerateMocks([AbstractProfilesRepository])
@@ -15,33 +19,130 @@ void main() {
 
     setUp(() {
       mockProfilesRepository = MockAbstractProfilesRepository();
-      sut = CreatedProfileScreenModel(profilesRepository: mockProfilesRepository);
+      when(mockProfilesRepository.loadProfile()).thenAnswer((_) async => null);
+      sut = CreatedProfileScreenModel(
+        profilesRepository: mockProfilesRepository,
+      );
     });
 
-    test('loadProfile returns profile from repository', () async {
+    // MARK: - profileFuture
+
+    test('profileFuture loads profile on construction', () async {
       // given
+      clearInteractions(mockProfilesRepository);
+
       final expectedProfile = ProfileDomainRandom.random();
-      when(mockProfilesRepository.loadProfile())
-          .thenAnswer((_) async => expectedProfile);
+      when(
+        mockProfilesRepository.loadProfile(),
+      ).thenAnswer((_) async => expectedProfile);
 
       // when
-      final profile = await sut.loadProfile();
+      final sut = CreatedProfileScreenModel(
+        profilesRepository: mockProfilesRepository,
+      );
+      final profile = await sut.profileFuture;
 
       // then
       expect(profile?.name, expectedProfile.name);
+      expect(profile?.profileImagePath, expectedProfile.profileImagePath);
+      expect(profile?.activities, expectedProfile.activities);
       verify(mockProfilesRepository.loadProfile()).called(1);
     });
 
-    test('loadProfile returns null when repository has no profile', () async {
-      // given
-      when(mockProfilesRepository.loadProfile()).thenAnswer((_) async => null);
-
+    test('profileFuture returns null when repository has no profile', () async {
       // when
-      final profile = await sut.loadProfile();
+      final profile = await sut.profileFuture;
 
       // then
       expect(profile, isNull);
       verify(mockProfilesRepository.loadProfile()).called(1);
+    });
+
+    // MARK: - reloadProfile
+
+    test('reloadProfile reloads and calls onStateChanged', () async {
+      // given
+      clearInteractions(mockProfilesRepository);
+      var onStateChangedCallCount = 0;
+      sut.onStateChanged = () => onStateChangedCallCount++;
+
+      final expectedProfile = ProfileDomainRandom.random();
+      when(
+        mockProfilesRepository.loadProfile(),
+      ).thenAnswer((_) async => expectedProfile);
+
+      // when
+      sut.reloadProfile();
+      final profile = await sut.profileFuture;
+
+      // then
+      expect(profile?.name, expectedProfile.name);
+      expect(profile?.profileImagePath, expectedProfile.profileImagePath);
+      expect(profile?.activities, expectedProfile.activities);
+      expect(onStateChangedCallCount, 1);
+      verify(mockProfilesRepository.loadProfile()).called(1);
+    });
+
+    // MARK: - selectedActivities
+
+    test('selectedActivities returns only selected activities', () {
+      // given
+      final profile = ProfileDomainRandom.random();
+      final expectedEntries = profile.activities.entries
+          .where((activity) => activity.value)
+          .map((activity) => MapEntry(activity.key.displayName, true));
+
+      // when
+      final result = sut.selectedActivities(profile);
+
+      // then
+      expect(result, Map.fromEntries(expectedEntries));
+    });
+
+    test('selectedActivities returns empty map for null profile', () {
+      // when
+      final result = sut.selectedActivities(null);
+
+      // then
+      expect(result, isEmpty);
+    });
+
+    // MARK: - getEditButtonAction
+
+    testWidgets('getEditButtonAction returns null when profile is null', (
+      WidgetTester tester,
+    ) async {
+      // given
+      final observer = TestNavigatorObserver();
+      final buildContext = await BuildContextProvider.getWithObserver(
+        tester,
+        observer,
+      );
+      final navigator = Navigator.of(buildContext);
+
+      // then
+      expect(sut.getEditButtonAction(null, navigator), isNull);
+    });
+
+    testWidgets('getEditButtonAction pushes EditProfileScreen', (
+      WidgetTester tester,
+    ) async {
+      // given
+      final profile = ProfileDomainRandom.random();
+      final observer = TestNavigatorObserver();
+      final buildContext = await BuildContextProvider.getWithObserver(
+        tester,
+        observer,
+      );
+      final navigator = Navigator.of(buildContext);
+
+      // when
+      sut.getEditButtonAction(profile, navigator)?.call();
+      await tester.pumpAndSettle();
+
+      // then
+      expect(observer.lastPushedRoute, isA<MaterialPageRoute>());
+      expect(find.byType(EditProfileScreen), findsOneWidget);
     });
   });
 }
